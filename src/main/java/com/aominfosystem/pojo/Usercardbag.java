c211da2c0e4ad20e2e1d7b66e721ef92fda52394
Keith400 <2353686862@qq.com>
2019-09-09 09:51:00 +0800

    提价Usercarbag实体类

---
+++ src/main/java/com/aominfosystem/pojo/Usercardbag.java
@@ -0,0 +1,57 @@
+package com.aominfosystem.pojo;
+
+
+public class Usercardbag {
+
+  private long id;
+  private long fromqq;
+  private String cardGroupName;
+  private String cardName;
+  private String cardDescribe;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getFromqq() {
+    return fromqq;
+  }
+
+  public void setFromqq(long fromqq) {
+    this.fromqq = fromqq;
+  }
+
+
+  public String getCardGroupName() {
+    return cardGroupName;
+  }
+
+  public void setCardGroupName(String cardGroupName) {
+    this.cardGroupName = cardGroupName;
+  }
+
+
+  public String getCardName() {
+    return cardName;
+  }
+
+  public void setCardName(String cardName) {
+    this.cardName = cardName;
+  }
+
+
+  public String getCardDescribe() {
+    return cardDescribe;
+  }
+
+  public void setCardDescribe(String cardDescribe) {
+    this.cardDescribe = cardDescribe;
+  }
+
+}
